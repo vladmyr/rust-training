959e1c8cb591c61bfdc987487f61341d7f1361e4
Volodymyr Khytskyi <khytsky.vladimir@gmail.com>
2019-01-03 23:59:21 +0100

    codewars -- Build a pile of Cubes #2

--- codewars_build_a_pile_of_cubes/src/main.rs
+++ codewars_build_a_pile_of_cubes/src/main.rs
@@ -1,4 +1,6 @@
 /**
+ * https://www.codewars.com/kata/build-a-pile-of-cubes/rust
+ * 
  * Your task is to construct a building which will be a pile of n cubes. The 
  * cube at the bottom will have a volume of n^3, the cube above will have volume
  * of (n-1)^3 and so on until the top which will have a volume of 1^3.
